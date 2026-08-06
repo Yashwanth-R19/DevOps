8688f2f93fd9a1556788d1edaf760e434382c2cd
Yashwanth-R19 <yashwanth.19.r@gmail.com>
2026-08-06 05:50:35 +0000

    Initial commit with Lab-ex-1

---
+++ Lab-Ex-1/linux_lab/script.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+echo "Welcome to Linux Lab"
+echo "Current User: $USER"
+echo "Current Working Directory: $(pwd)"
+echo "Current Date and Time: $(date)"
+echo "Course Name: $COURSE_NAME"
